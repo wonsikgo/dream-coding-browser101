29b73d9c6435bddf8717f3322c5656151abb7a58
wonsikgo <wonsickgo@gmail.com>
2021-05-09 01:08:44 +0900

    좌표 실습 성능 개선

--- coordinates/main.js
+++ coordinates/main.js
@@ -14,14 +14,16 @@ const vertical = document.querySelector('.vertical');
 const horozontal = document.querySelector('.horozontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
 document.addEventListener('mousemove', (event) => {
   const x = event.clientX;
   const y = event.clientY;
-  vertical.style.left = `${x}px`;
-  horozontal.style.top = `${y}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
+  vertical.style.transform = `translateX(${x}px)`;
+  horozontal.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
   tag.innerHTML = `${x}px, ${y}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
 });
