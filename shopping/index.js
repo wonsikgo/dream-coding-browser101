a015ff4c8d9653e09fc64f7dffa6de36a1e6806b
wonsikgo <wonsickgo@gmail.com>
2021-05-13 00:40:19 +0900

    DOM 실습 코드 리팩토링

--- shopping/index.js
+++ shopping/index.js
@@ -3,24 +3,38 @@ window.onload = () => {
   const input = document.querySelector('footer input');
   const ul = document.querySelector('section ul');
   addBtn.addEventListener('click', () => {
-    if (!input.value) return;
-    addItem(input.value);
+    if (!input.value) {
+      input.focus();
+      return;
+    }
+    onAdd(input.value);
+  });
+  input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      if (!input.value) {
+        input.focus();
+        return;
+      }
+      onAdd(input.value);
+    }
   });
 
-  function addItem(item) {
+  function onAdd(item) {
     const li = document.createElement('li');
     const span = document.createElement('span');
     const i = document.createElement('i');
     li.className = 'list';
     i.className = 'fas fa-trash-alt';
-    i.addEventListener('click', (event) => {
-      ul.removeChild(event.target.parentNode);
+    i.addEventListener('click', () => {
+      ul.removeChild(li);
     });
     span.innerText = item;
     li.appendChild(span);
     li.appendChild(i);
 
     ul.appendChild(li);
+    li.scrollIntoView({ block: 'center' });
     input.value = '';
+    input.focus();
   }
 };
