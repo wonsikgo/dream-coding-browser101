f266a61950942a077c0c8a3bc5ae1f0ceddb8df9
wonsikgo <wonsickgo@gmail.com>
2021-05-14 00:12:26 +0900

    이벤트 버블링, 캡쳐링, preventDefault

--- shopping/index.js
+++ shopping/index.js
@@ -1,40 +1,39 @@
-window.onload = () => {
-  const addBtn = document.querySelector('footer button');
-  const input = document.querySelector('footer input');
-  const ul = document.querySelector('section ul');
-  addBtn.addEventListener('click', () => {
+window.onload = () => {};
+const addBtn = document.querySelector('footer button');
+const input = document.querySelector('footer input');
+const ul = document.querySelector('section ul');
+addBtn.addEventListener('click', () => {
+  if (!input.value) {
+    input.focus();
+    return;
+  }
+  onAdd(input.value);
+});
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
     if (!input.value) {
       input.focus();
       return;
     }
     onAdd(input.value);
-  });
-  input.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-      if (!input.value) {
-        input.focus();
-        return;
-      }
-      onAdd(input.value);
-    }
-  });
-
-  function onAdd(item) {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const i = document.createElement('i');
-    li.className = 'list';
-    i.className = 'fas fa-trash-alt';
-    i.addEventListener('click', () => {
-      ul.removeChild(li);
-    });
-    span.innerText = item;
-    li.appendChild(span);
-    li.appendChild(i);
-
-    ul.appendChild(li);
-    li.scrollIntoView({ block: 'center' });
-    input.value = '';
-    input.focus();
   }
-};
+});
+
+function onAdd(item) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const i = document.createElement('i');
+  li.className = 'list';
+  i.className = 'fas fa-trash-alt';
+  i.addEventListener('click', () => {
+    ul.removeChild(li);
+  });
+  span.innerText = item;
+  li.appendChild(span);
+  li.appendChild(i);
+
+  ul.appendChild(li);
+  li.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
+}
