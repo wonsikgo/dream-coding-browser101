ddd378ecef4acb41abb3818522d2dd0454c5229b
wonsikgo <wonsickgo@gmail.com>
2021-05-15 23:03:24 +0900

    이벤트 위임 리팩토링

--- shopping/index.js
+++ shopping/index.js
@@ -18,22 +18,36 @@ input.addEventListener('keypress', (event) => {
     onAdd(input.value);
   }
 });
+ul.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.list[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
 
+let id = 0;
 function onAdd(item) {
+  // const span = document.createElement('span');
+  // const i = document.createElement('i');
+  // i.className = 'fas fa-trash-alt';
+  // i.addEventListener('click', () => {
+  //   ul.removeChild(li);
+  // });
+  // span.innerText = item;
+  // li.appendChild(span);
+  // li.appendChild(i);
   const li = document.createElement('li');
-  const span = document.createElement('span');
-  const i = document.createElement('i');
   li.className = 'list';
-  i.className = 'fas fa-trash-alt';
-  i.addEventListener('click', () => {
-    ul.removeChild(li);
-  });
-  span.innerText = item;
-  li.appendChild(span);
-  li.appendChild(i);
+  li.dataset.id = id;
+  li.innerHTML = `
+  <span>${item}</span>
+  <i class="fas fa-trash-alt remove-btn" data-id="${id}"></i>
+  `;
 
   ul.appendChild(li);
   li.scrollIntoView({ block: 'center' });
   input.value = '';
   input.focus();
+  id++;
 }
