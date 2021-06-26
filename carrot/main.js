59dc5c818e721f770f51b70bb229fc07c3b5de7f
wonsikgo <wonsickgo@gmail.com>
2021-06-27 01:18:21 +0900

    당근찾기 스타일링 수정

---
+++ carrot/main.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  const playBtn = document.querySelector('.play');
+  const stopbTN = document.querySelector('.stop');
+
+  playBtn.addEventListener('click', () => {
+    console.log('11');
+  });
+
+  stopbTN.addEventListener('click', () => {
+    console.log('11');
+  });
+};
