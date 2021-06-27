05aaf754d72f0a0357579212c3c4fd03ab50f8ca
wonsikgo <wonsickgo@gmail.com>
2021-06-27 19:48:30 +0900

    당근찾기, 리플래시 이벤트 추가

--- carrot/main.js
+++ carrot/main.js
@@ -1,29 +1,52 @@
 window.onload = () => {
-  const html = document.querySelector('html');
   const playBtn = document.querySelector('.play');
-  const stopbTN = document.querySelector('.stop');
+  const stopBtn = document.querySelector('.stop');
+  const reflashBtn = document.querySelector('.reflash');
+
   const timerCount = document.querySelector('.timer-count');
+  const carrotCount = document.querySelector('.carrot-count');
   const field = document.querySelector('.field');
+  const reflashPopup = document.querySelector('.reflash-popup');
+
+  const wonComment = document.querySelector('.won-comment');
+  const lostComment = document.querySelector('.lost-comment');
+  const replyComment = document.querySelector('.reply-comment');
+
+  const carrot = document.querySelector('.carrot');
+  const bug = document.querySelector('.bug');
+
   const browserWidth = document.documentElement.clientWidth - 80;
   const browserHeight = document.documentElement.clientHeight - 80;
-  console.log(`browserWidth : ${browserWidth}`);
-  console.log(`browserHeight : ${browserHeight}`);
+  // console.log(`browserWidth : ${browserWidth}`);
+  // console.log(`browserHeight : ${browserHeight}`);
   console.log(Math.floor(Math.random() * (browserHeight - 500 + 1) + 500));
   let count = 10;
   let isPlayState = true;
 
   playBtn.addEventListener('click', () => {
     playBtn.classList.add('hidden');
-    stopbTN.classList.remove('hidden');
+    stopBtn.classList.remove('hidden');
     timerCount.innerHTML = count;
     countDown();
     setImagePosition();
   });
 
-  stopbTN.addEventListener('click', () => {
-    stopbTN.classList.add('hidden');
-    playBtn.classList.remove('hidden');
+  stopBtn.addEventListener('click', () => {
+    stopBtn.classList.add('visibility');
+    reflashPopup.classList.remove('hidden');
+    replyComment.classList.remove('hidden');
     isPlayState = false;
+  });
+
+  reflashBtn.addEventListener('click', () => {
+    field.innerHTML = '';
+    stopBtn.classList.remove('visibility');
+    reflashPopup.classList.add('hidden');
+    count = 10;
+    timerCount.innerHTML = count;
+    isPlayState = true;
+    countDown();
+    setImagePosition();
   });
 
   function countDown() {
@@ -37,17 +60,42 @@ window.onload = () => {
 
   function setImagePosition() {
     for (let i = 0; i < 10; i++) {
-      const parrat = document.createElement('img');
-      const x = randomWidthPosition();
-      const y = randomHeightPosition();
-      console.log(`x : ${x}`);
-      console.log(`y : ${y}`);
-      parrat.setAttribute('src', './img/carrot.png');
-      parrat.style.position = 'absolute';
-      parrat.style.top = `${y}px`;
-      parrat.style.left = `${x}px`;
-      field.appendChild(parrat);
+      const carrot = createCarrot();
+      const bug = createBug();
+      field.appendChild(carrot);
+      field.appendChild(bug);
     }
+  }
+
+  function createCarrot() {
+    const carrot = document.createElement('img');
+    const x = randomWidthPosition();
+    const y = randomHeightPosition();
+    // console.log(`x : ${x}`);
+    // console.log(`y : ${y}`);
+    carrot.setAttribute('src', './img/carrot.png');
+    carrot.setAttribute('class', 'carrot');
+    carrot.style.position = 'absolute';
+    carrot.style.top = `${y}px`;
+    carrot.style.left = `${x}px`;
+    carrot.addEventListener('click', (e) => {
+      field.removeChild(e.target);
+    });
+    return carrot;
+  }
+
+  function createBug() {
+    const bug = document.createElement('img');
+    const x = randomWidthPosition();
+    const y = randomHeightPosition();
+    // console.log(`x : ${x}`);
+    // console.log(`y : ${y}`);
+    bug.setAttribute('src', './img/bug.png');
+    bug.setAttribute('class', 'bug');
+    bug.style.position = 'absolute';
+    bug.style.top = `${y}px`;
+    bug.style.left = `${x}px`;
+    return bug;
   }
 
   function randomWidthPosition() {
