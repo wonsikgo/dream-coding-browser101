4bc75ecc6f693dc46add7717d0b7b456ad9333aa
wonsikgo <wonsickgo@gmail.com>
2021-06-29 02:16:39 +0900

    당근찾기 리팩토링, 변수 상수화

--- carrot-minigame/src/game.js
+++ carrot-minigame/src/game.js
@@ -1,5 +1,5 @@
 'use strict';
-import Field from './field.js';
+import { Field, ItemType } from './field.js';
 import * as sound from './sound.js';
 
 export const Reason = Object.freeze({
@@ -60,13 +60,13 @@ class Game {
     if (!this.started) {
       return;
     }
-    if (item === 'carrot') {
+    if (item === ItemType.carrot) {
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCound) {
         this.stop(Reason.win);
       }
-    } else if (item === 'bug') {
+    } else if (item === ItemType.bug) {
       this.stop(Reason.lose);
     }
   };
