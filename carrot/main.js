07302e5d27998405fabb2bde1413c5279deb7e70
wonsikgo <wonsickgo@gmail.com>
2021-07-04 23:53:57 +0900

    당근찾기 리팩토링, 필요없는코드 제거

--- carrot/main.js
+++ carrot/main.js
@@ -13,10 +13,6 @@ const wonComment = document.querySelector('.won-comment');
 const lostComment = document.querySelector('.lost-comment');
 const replyComment = document.querySelector('.reply-comment');
 
-// const bugSound = document.querySelector('.bug-sound');
-// const carrotSound = document.querySelector('.carrot-sound');
-// const gameWinSound = document.querySelector('.game-win-sound');
-
 const browserWidth = document.documentElement.clientWidth - 80;
 const browserHeight = document.documentElement.clientHeight - 80;
 
@@ -26,55 +22,60 @@ const bgSound = new Audio('./sound/bg.mp3');
 const alertSound = new Audio('./sound/alert.wav');
 const winSound = new Audio('./sound/game_win.mp3');
 
-// console.log(`browserWidth : ${browserWidth}`);
-// console.log(`browserHeight : ${browserHeight}`);
-console.log(Math.floor(Math.random() * (browserHeight - 500 + 1) + 500));
-let count = 10;
-let carrotCnt = 0;
+let COUNT = 10;
+let SCORE = 0;
 let isPlayState = true;
 
 playBtn.addEventListener('click', () => {
-  playBtn.classList.add('hidden');
-  stopBtn.classList.remove('hidden');
-  timerCount.innerHTML = count;
-  carrotCount.innerHTML = carrotCnt;
-  countDown();
-  setImagePosition();
-  bgSound.play();
+  startGame();
 });
 
 stopBtn.addEventListener('click', () => {
-  stopBtn.classList.add('visibility');
-  refleshPopup.classList.remove('hidden');
-  hideComment();
-  replyComment.classList.remove('hidden');
-  isPlayState = false;
-  alertSound.play();
+  endGame();
 });
 
 refleshBtn.addEventListener('click', () => {
   field.innerHTML = '';
   stopBtn.classList.remove('visibility');
   refleshPopup.classList.add('hidden');
-  count = 10;
-  carrotCnt = 0;
-  timerCount.innerHTML = count;
-  carrotCount.innerHTML = carrotCnt;
+  COUNT = 10;
+  SCORE = 0;
+  timerCount.innerHTML = COUNT;
+  carrotCount.innerHTML = SCORE;
   isPlayState = true;
   countDown();
   setImagePosition();
   bgSound.play();
 });
 
+function startGame() {
+  playBtn.classList.add('hidden');
+  stopBtn.classList.remove('hidden');
+  timerCount.innerHTML = COUNT;
+  carrotCount.innerHTML = SCORE;
+  countDown();
+  setImagePosition();
+  bgSound.play();
+}
+
+function endGame() {
+  stopBtn.classList.add('visibility');
+  refleshPopup.classList.remove('hidden');
+  hideComment();
+  replyComment.classList.remove('hidden');
+  isPlayState = false;
+  alertSound.play();
+}
+
 function countDown() {
   setTimeout(() => {
-    if (count === 0 && carrotCnt < 10) {
+    if (COUNT === 0 && SCORE < 10) {
       onClickBug();
       return;
     }
-    if (count < 1 || !isPlayState) return;
-    count = count - 1;
-    timerCount.innerHTML = count;
+    if (COUNT < 1 || !isPlayState) return;
+    COUNT = COUNT - 1;
+    timerCount.innerHTML = COUNT;
     countDown();
   }, 1000);
 }
@@ -131,10 +132,10 @@ function randomHeightPosition() {
 }
 
 function onClickCarrot() {
-  carrotCnt = carrotCnt + 1;
-  carrotCount.innerHTML = carrotCnt;
+  SCORE = SCORE + 1;
+  carrotCount.innerHTML = SCORE;
   carrotSound.play();
-  if (carrotCnt === 10 && count >= 0) {
+  if (SCORE === 10 && COUNT >= 0) {
     gameWinSound.play();
     isPlayState = false;
     stopBtn.classList.add('visibility');
